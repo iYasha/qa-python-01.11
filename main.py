075dd704d53a6972a46aa30e65bbb471e864c973
iYasha <33287747+iYasha@users.noreply.github.com>
2022-11-02 00:08:00 +0200

    01.11

---
+++ main.py
@@ -0,0 +1,90 @@
+
+'Hello \' world'  # str
+"He\"llo world"  # str
+10  # int
+1.5  # float
+True  # bool (True - 1 | False - 0)
+(1, 5, 5, 'Hello world', True, False)  # tuple
+[1, 5, 5, 'Hello world', True, False, [1, 2]]  # list
+{ 1, 2, 3, 3, 3, 4, 2 }  # set
+dictionary = {
+    'admin': '1234',
+    'vasya': 'qwerty'
+}
+lists = [
+    'admin', '1234', 'vasya', 'qwerty'
+]
+
+
+"""
+Что делает ?
+Выводит
+Зачем ?
+Потому что могу
+"""
+
+# print('Hello world', 10, 1.5, True)  # Эта строка выводит какие-то значения
+# print( 5 < 5 )
+# print( 5 > 5 )
+# print( 5 <= 5 )
+# print( 20 >= 5 )
+# print( 20 == 5 )
+# print( 20 != 5 )
+# print('Hello' + ' Petr!')
+# print('Hello' * 30)
+# print(True + True)
+# строка №1
+# строка №2
+
+# print( 10.5 + 2 )
+# print( 10.3 - 2 )
+# print( 10.2 / 2 )
+# print( 10.6 * 2 )
+
+# print( 0.2 + 0.8 )
+# print( 0.1 + 0.1 + 0.1 )
+
+# names = ['Petr', 'Sasha', 'Vasya']
+#
+# print(names)
+#
+# new_name = input('Enter new name: ')
+#
+# names.append(new_name)
+#
+# print(names)
+
+
+# print(2 + 2)
+#
+# name = input('Enter your name: ')
+#
+# print('Text after input', name)
+
+# print(1,2,3,4,5,6, sep='__+', end='')
+# print('Hello', 'world\n\t\t!!!', sep='__+', end='')
+# input('Enter your number: ')
+
+
+a = 'Hello world'
+b = 222
+# c = a + b
+
+print('B = ', b)
+
+# CONST_VALUE = 'secret key'
+#
+# print(CONST_VALUE)
+#
+# CONST_VALUE = 123
+#
+# print(CONST_VALUE)
+
+name = input('Enter your name: ')
+b = None
+print('B = ', b)
+c = print('Hello ' + name + '!')
+print('#76: ', c)
+
+user_email = 'admin@admin.com'
+
